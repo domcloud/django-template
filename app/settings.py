b4c49bb44eb350d88a94bc18c94500e1e6bf120c
Wildan M <willnode@wellosoft.net>
2020-09-23 00:37:14 +0700

    Use MySQL

--- app/settings.py
+++ app/settings.py
@@ -16,17 +16,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'database_here',
-        'USER': 'username_here',
-        'PASSWORD': 'password_here',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -91,8 +80,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'database_here',
+        'USER': 'username_here',
+        'PASSWORD': 'password_here',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
